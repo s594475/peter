5c871c5ffda841550db9b66ba3d7334a4064b5c8
s594475 <543802872@qq.com>
2016-08-06 15:49:21 +0800

    three

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import map from 'lodash/fp/map';
-
+import axios from 'axios';
 
 import BlogCard from './BlogCard.js';
 
-let blogs = [
-  {index: '1', title:'这是第一天', date:'2016.7.19'},
-  {index: '2', title:'这是第二天', date:'2016.7.21'},
-  {index: '3', title:'这是第三天', date:'2016.7.22'}
-]
 
-
+//https://raw.githubusercontent.com/s594475/peter/master/posts/index.json
 class List extends Component {
+  constructor(){
+    super();
+    this.state={
+      posts: ''
+    }
+  }
+
+  componentDidMount(){
+    let address = `https://raw.githubusercontent.com/s594475/peter/master/posts/index.json?v=${Math.random()}`
+    axios.get(address).then((res) => {
+      this.setState({
+        posts: res.data
+      });
+    });
+  }
+
   render(){
     // console.log(Cards.length);
     var blogCards = [];
@@ -20,7 +31,7 @@ class List extends Component {
                     <BlogCard title={b.title} date={b.date} index={b.index} key={Math.random()}/>
                   );
                 },
-        blogs
+        this.state.posts
     );
     // console.log(AllCards);
     return(
