6b9860c1ba307d5627977e1b04aa6939d8b5da9b
s594475 <543802872@qq.com>
2016-08-07 11:57:26 +0800

    8-7

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,15 +1,37 @@
-import React, { PropTypes } from 'react'
-
+import React, { PropTypes } from 'react';
+import axios from 'axios';
+import marked from 'marked';
 class Post extends React.Component {
-  render () {
+  constructor(){
+    super();
+    this.state={
+      text: ''
+    }
+  }
+
+  componentDidMount(){
+      console.log("enenn")
+    let address = `https://raw.githubusercontent.com/happypeter/big-demo/master/posts/git-tips.md?v=${Math.random()}`
+    axios.get(address).then((res) => {
+
+      this.setState({
+        text: res.data
+      });
+    });
+
+  }
+
+  render(){
+    let content = marked(this.state.text!='' ? this.state.text : '稍等.....')
     return(
       <div>
-        {
-          this.props.params.title
-        }
+          {this.props.params.title}
+          <div>
+               <span dangerouslySetInnerHTML={{__html: content}} />
+          </div>
       </div>
     )
   }
-}
 
+}
 export default Post;
